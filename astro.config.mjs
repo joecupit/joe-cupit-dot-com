fbf9a98fecfdd3892a7bf45e7e659e07ec921139
Joe <jwcupit@gmail.com>
2025-12-14 17:29:32 +0000

    Update to custom domain and add 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://joe-cupit.github.io",
-  base: "/joe-cupit-dot-com",
+  site: "https://joecupit.com",
 });
