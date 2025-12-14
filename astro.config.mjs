3c5ba83ca69ef71efacae8f8ee6c065a5683978f
Joe <jwcupit@gmail.com>
2025-12-14 14:38:58 +0000

    Add GitHub pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://joe-cupit.github.io",
+  base: "/joe-cupit-dot-com",
+});
