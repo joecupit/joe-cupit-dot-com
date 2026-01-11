aca5803c529cafafff22dab46ded067270ee668b
Joe <jwcupit@gmail.com>
2026-01-11 15:57:23 +0000

    Add project collection and page layout

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const projects = defineCollection({
+  loader: glob({
+    base: "./src/content/projects",
+    pattern: "**/*.{md,mdx}",
+  }),
+
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      subtitle: z.string(),
+      url: z.string().url(),
+      coverImage: image().optional(),
+    }),
+});
+
+export const collections = { projects };
---
+++ src/content/projects/wainroutes.md
@@ -0,0 +1,27 @@
+---
+title: Wainroutes
+subtitle: Personal project
+url: https://wainroutes.co.uk
+---
+
+![Wainroutes home page](./_images/wainroutes-1.png)
+
+## Summary
+
+Something summarising.
+
+## Goals
+
+About the goals.
+
+![Wainroutes walks page](./_images/wainroutes-3.png)
+
+## Technical stuff
+
+Some technical descriptions.
+
+![Wainroutes weather page](./_images/wainroutes-2.png)
+
+## Roles
+
+What I did.
